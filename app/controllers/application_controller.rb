6b2676a15f33d5a170ed1e50f36eb48a48e0ca6c
Andrea T <tostonar@gmail.com>
2019-09-17 13:18:33 -0400

    can update article

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ require_relative '../../config/environment'
 require 'pry'
 
 class ApplicationController < Sinatra::Base
+  
 
   configure do
     set :public_folder, 'public'
@@ -24,11 +25,25 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  get '/articles/:id/edit' do
+    id = params[:id]
+    @article = Article.find(id)
+    erb :edit
+  end
+
   post '/articles' do
     title = params[:title]
     content = params[:content]
     a = Article.create(title: title, content: content)
     # binding.pry
     redirect "/articles/#{a.id}"
+  end
+
+  patch '/articles/:id' do
+    article = Article.find(params[:id])
+    title = params[:title]
+    content = params[:content]
+    article.update(title: title, content: content)
+    redirect "articles/#{article.id}"
   end
 end
