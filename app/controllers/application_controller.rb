b430dc4866921d4089d9f0d2071cffb7ae4be85e
Andrea T <tostonar@gmail.com>
2019-09-17 11:43:36 -0400

    Create article

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,18 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/article/new' do 
+    erb :new
+  end
+
+  post '/articles' do
+    title = params[:title]
+    content = params[:content]
+    Article.create(title: title, content: content)
+    redirect '/'
   end
 end
